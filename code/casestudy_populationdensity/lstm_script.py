9ce016967f8482b98dee94054dad0f92aec2f817
MiguelGarcaoSilva <miguelgarcaosilva@gmail.com>
2025-02-04 15:21:40 +0000

    updated t-tests

--- code/casestudy_populationdensity/lstm_script.py
+++ code/casestudy_populationdensity/lstm_script.py
@@ -41,6 +41,7 @@ mp_stats_table = mp_stats_table[mp_stats_table["m"] == MOTIF_SIZE]
 top_motifs = mp_stats_table.sort_values(by="#Matches", ascending=False).head(NTOP_MOTIFS)
 top_motifs = top_motifs[["m", "Indices"]]
 
+
 # %%
 # read csv
 data_df = pd.read_csv(
@@ -69,7 +70,7 @@ test_tensor()
 from utils.utils import create_dataset
 from utils.train_pipeline import run_optuna_study
 from utils.utils import get_best_model_results_traindevtest, plot_best_model_results_traindevtest
-from models.transformer_pytorch import TCN
+from models.lstm_pytorch import LSTM
 from utils.utils import plot_preds_vs_truevalues
 from utils.train_pipeline import get_preds_best_config_train_val_test
 
@@ -85,7 +86,7 @@ for i, top_motif in top_motifs.iterrows():
 
     motif_indexes = sorted(ast.literal_eval(top_motif["Indices"]))
     
-    print(f"Evaluating motif {i+1} with size {MOTIF_SIZE} and {len(motif_indexes)}  indexes")
+    print(f"Evaluating motif {i+1} with size {MOTIF_SIZE} and {len(motif_indexes)} indexes")
     
     # Create dataset for the current motif
     X_series, X_indices, X_mask, y = create_dataset(data, lookback_period, step, forecast_period, motif_indexes, MOTIF_SIZE)
@@ -99,26 +100,18 @@ for i, top_motif in top_motifs.iterrows():
     # Define the model and run the Optuna study
     n_trials = 100
     num_epochs = 500
-    model_type = "TCN"
-    model_name = "TCNSeries"
+    model_type = "LSTM"
+    model_name = "LSTM_Series"
 
     suggestion_dict = {
         "learning_rate": {
             "type": "float",
-            "args": [1e-5, 1e-3],
-            "kwargs": {"log": True}
+            "args": [1e-5, 1e-3], 
+            "kwargs": {"log": True} 
         },
-        "kernel_size": { # ensure receptive field is at least as large as sequence length (lookback_period)
+        "num_layers": {
             "type": "categorical",
-            "args": [[3, 5 ,7]]
-        },
-        "receptive_field": {
-            "type": "categorical",
-            "args": [[50]]
-        },
-        "dropout": {
-            "type": "float",
-            "args": [0.0, 0.5]
+            "args": [[1,3,5]]  
         },
         "batch_size": {
             "type": "categorical",
@@ -126,13 +119,13 @@ for i, top_motif in top_motifs.iterrows():
         }
     }
 
+    model_params_keys = ["hidden_sizes_list"]
 
-    model_params_keys = ["kernel_size", "num_channels_list", "dropout"]
     model_results_dir = os.path.join(RESULTS_DIR, f"{model_name}_{n_trials}_trials_{num_epochs}_epochs_motif_{i+1}")
     os.makedirs(model_results_dir, exist_ok=True)  
 
     X = {"X_series": X_series}
-    run_optuna_study(pipeline.run_train_val_test, Transformer, model_type, suggestion_dict, model_params_keys, seed, X, y, NORMALIZE_FLAGS, model_results_dir, n_trials=n_trials, num_epochs=num_epochs)
+    run_optuna_study(pipeline.run_train_val_test, eval(model_type), model_type, suggestion_dict, model_params_keys, seed, X, y, NORMALIZE_FLAGS, model_results_dir, n_trials=n_trials, num_epochs=num_epochs)
 
     study = joblib.load(os.path.join(model_results_dir, "study.pkl"))
     train_losses, val_losses, best_epoch, test_loss, test_mae, test_rmse = get_best_model_results_traindevtest(study)
@@ -144,7 +137,7 @@ for i, top_motif in top_motifs.iterrows():
     test_mae_list.append(test_mae)
     test_rmse_list.append(test_rmse)
     
-    epochs_train_losses, epochs_val_losses, val_losses, test_losses, test_mae, test_rmse, all_predictions, all_true_values = get_preds_best_config_train_val_test(study, pipeline,Transformerpe, model_params_keys, num_epochs=num_epochs, seed=seed, X=X, y=y, normalize_flags=NORMALIZE_FLAGS)
+    epochs_train_losses, epochs_val_losses, val_losses, test_losses, test_mae, test_rmse, all_predictions, all_true_values = get_preds_best_config_train_val_test(study, pipeline, eval(model_type), model_type, model_params_keys, num_epochs=num_epochs, seed=seed, X=X, y=y, normalize_flags=NORMALIZE_FLAGS)
     plot_best_model_results_traindevtest( study.trials_dataframe(),
         save_path=os.path.join(IMAGES_DIR, f"{model_name}_{n_trials}_trials_{num_epochs}_epochs_motif_{i+1}_best_results.png")
     )    
@@ -177,7 +170,7 @@ print(f"Mean Test RMSE: {mean_test_rmse} ± {std_test_rmse}")
 from utils.utils import create_dataset
 from utils.train_pipeline import run_optuna_study
 from utils.utils import get_best_model_results_traindevtest, plot_best_model_results_traindevtest
-from models.transformer_pytorch import TCN
+from models.lstm_pytorch import LSTM
 from utils.utils import plot_preds_vs_truevalues
 from utils.train_pipeline import get_preds_best_config_train_val_test
 
@@ -193,7 +186,7 @@ for i, top_motif in top_motifs.iterrows():
 
     motif_indexes = sorted(ast.literal_eval(top_motif["Indices"]))
     
-    print(f"Evaluating motif {i+1} with size {MOTIF_SIZE} and {len(motif_indexes)}  indexes")
+    print(f"Evaluating motif {i+1} with size {MOTIF_SIZE} and {len(motif_indexes)} indexes")
     
     # Create dataset for the current motif
     X_series, X_indices, X_mask, y = create_dataset(data, lookback_period, step, forecast_period, motif_indexes, MOTIF_SIZE)
@@ -207,26 +200,18 @@ for i, top_motif in top_motifs.iterrows():
     # Define the model and run the Optuna study
     n_trials = 100
     num_epochs = 500
-    model_type = "TCN"
-    model_name = "TCNSeries_Masking"
+    model_type = "LSTM"
+    model_name = "LSTM_Series_Masking"
 
     suggestion_dict = {
         "learning_rate": {
             "type": "float",
-            "args": [1e-5, 1e-3],
-            "kwargs": {"log": True}
+            "args": [1e-5, 1e-3], 
+            "kwargs": {"log": True} 
         },
-        "kernel_size": { # ensure receptive field is at least as large as sequence length (lookback_period)
+        "num_layers": {
             "type": "categorical",
-            "args": [[3, 5 ,7]]
-        },
-        "receptive_field": {
-            "type": "categorical",
-            "args": [[50]]
-        },
-        "dropout": {
-            "type": "float",
-            "args": [0.0, 0.5]
+            "args": [[1,3,5]]  
         },
         "batch_size": {
             "type": "categorical",
@@ -234,13 +219,113 @@ for i, top_motif in top_motifs.iterrows():
         }
     }
 
+    model_params_keys = ["hidden_sizes_list"]
 
-    model_params_keys = ["kernel_size", "num_channels_list", "dropout"]
     model_results_dir = os.path.join(RESULTS_DIR, f"{model_name}_{n_trials}_trials_{num_epochs}_epochs_motif_{i+1}")
     os.makedirs(model_results_dir, exist_ok=True)  
 
     X = {"X_series": X_series, "X_mask": X_mask}
-    run_optuna_study(pipeline.run_train_val_test, Transformerpe, suggestion_dict, model_params_keys, seed, X, y, NORMALIZE_FLAGS, model_results_dir, n_trials=n_trials, num_epochs=num_epochs)
+    run_optuna_study(pipeline.run_train_val_test, eval(model_type), model_type, suggestion_dict, model_params_keys, seed, X, y, NORMALIZE_FLAGS, model_results_dir, n_trials=n_trials, num_epochs=num_epochs)
+
+    study = joblib.load(os.path.join(model_results_dir, "study.pkl"))
+    train_losses, val_losses, best_epoch, test_loss, test_mae, test_rmse = get_best_model_results_traindevtest(study)
+
+    print(f"Best epoch: {best_epoch}")
+    print(f"Test Loss: {test_loss}, Test MAE: {test_mae}, Test RMSE: {test_rmse}")
+
+    test_losses_list.append(test_loss)
+    test_mae_list.append(test_mae)
+    test_rmse_list.append(test_rmse)
+    
+    epochs_train_losses, epochs_val_losses, val_losses, test_losses, test_mae, test_rmse, all_predictions, all_true_values = get_preds_best_config_train_val_test(study, pipeline, eval(model_type), model_type, model_params_keys, num_epochs=num_epochs, seed=seed, X=X, y=y, normalize_flags=NORMALIZE_FLAGS)
+    plot_best_model_results_traindevtest( study.trials_dataframe(),
+        save_path=os.path.join(IMAGES_DIR, f"{model_name}_{n_trials}_trials_{num_epochs}_epochs_motif_{i+1}_best_results.png")
+    )    
+    plot_preds_vs_truevalues(np.ravel(all_true_values), np.ravel(all_predictions), fold=0, save_path=os.path.join(IMAGES_DIR, f"{model_name}_{n_trials}_trials_{num_epochs}_epochs_motif_{i+1}_fold_{0}_predictions.png"))
+
+
+# Convert lists to numpy arrays for easier calculations
+test_losses_array = np.array(test_losses_list)
+test_mae_array = np.array(test_mae_list)
+test_rmse_array = np.array(test_rmse_list)
+
+# Calculate mean and standard deviation
+mean_test_loss = np.mean(test_losses_array)
+std_test_loss = np.std(test_losses_array)
+
+mean_test_mae = np.mean(test_mae_array)
+std_test_mae = np.std(test_mae_array)
+
+mean_test_rmse = np.mean(test_rmse_array)
+std_test_rmse = np.std(test_rmse_array)
+
+# Print aggregated results
+print(f"Aggregated Results Across Top 5 Motifs:")
+print(f"Mean Test Loss: {mean_test_loss} ± {std_test_loss}")
+print(f"Mean Test MAE: {mean_test_mae} ± {std_test_mae}")
+print(f"Mean Test RMSE: {mean_test_rmse} ± {std_test_rmse}")
+
+
+# %%
+from utils.utils import create_dataset
+from utils.train_pipeline import run_optuna_study
+from utils.utils import get_best_model_results_traindevtest, plot_best_model_results_traindevtest
+from models.lstm_pytorch import LSTM
+from utils.utils import plot_preds_vs_truevalues
+from utils.train_pipeline import get_preds_best_config_train_val_test
+
+lookback_period = 24*7*3 #window size
+step = 1 #step size for the sliding window
+forecast_period = 24*2 #forward window size
+test_losses_list = []
+test_mae_list = []
+test_rmse_list = []
+
+# Loop through each of the top 10 motifs
+for i, top_motif in top_motifs.iterrows():
+
+    motif_indexes = sorted(ast.literal_eval(top_motif["Indices"]))
+    
+    print(f"Evaluating motif {i+1} with size {MOTIF_SIZE} and {len(motif_indexes)} indexes")
+    
+    # Create dataset for the current motif
+    X_series, X_indices, X_mask, y = create_dataset(data, lookback_period, step, forecast_period, motif_indexes, MOTIF_SIZE)
+
+    # X_series, X2, and y are now PyTorch tensors
+    print("X_series shape:", X_series.shape)  # Expected shape: (num_samples, lookback_period, num_features)
+    print("X_indices shape:", X_indices.shape)  # Expected shape: (num_samples, max_motif_length_in_window, 1)
+    print("X_mask shape:", X_mask.shape)  # Expected shape: (num_samples, max_motif_length_in_window)
+    print("y shape:", y.shape)    # Expected shape: (num_samples, 1)
+    
+    # Define the model and run the Optuna study
+    n_trials = 100
+    num_epochs = 500
+    model_type = "LSTM"
+    model_name = "LSTM_Indexes"
+
+    suggestion_dict = {
+        "learning_rate": {
+            "type": "float",
+            "args": [1e-5, 1e-3], 
+            "kwargs": {"log": True} 
+        },
+        "num_layers": {
+            "type": "categorical",
+            "args": [[1,3,5]]  
+        },
+        "batch_size": {
+            "type": "categorical",
+            "args": [[4, 8, 16, 32]]
+        }
+    }
+
+    model_params_keys = ["hidden_sizes_list"]
+
+    model_results_dir = os.path.join(RESULTS_DIR, f"{model_name}_{n_trials}_trials_{num_epochs}_epochs_motif_{i+1}")
+    os.makedirs(model_results_dir, exist_ok=True)  
+
+    X = {"X_indices": X_indices}
+    run_optuna_study(pipeline.run_train_val_test, eval(model_type), model_type, suggestion_dict, model_params_keys, seed, X, y, NORMALIZE_FLAGS, model_results_dir, n_trials=n_trials, num_epochs=num_epochs)
 
     study = joblib.load(os.path.join(model_results_dir, "study.pkl"))
     train_losses, val_losses, best_epoch, test_loss, test_mae, test_rmse = get_best_model_results_traindevtest(study)
